35f693dfd98a1c4b244ee756c7eaee6079b763ba
Amani T <136659501+amanitristan@users.noreply.github.com>
2023-08-02 04:05:47 +0000

    Updated randomized numbers

--- dice.rb
+++ dice.rb
@@ -14,8 +14,8 @@ get("/giraffe") do
 end
 
 get("/dice/2/6") do
-  first_die = rand(7)
-  second_die = rand(7)
+  first_die = rand(1..6)
+  second_die = rand(1..6)
   sum = first_die + second_die
 
   outcome = "You rolled a " + first_die.to_s + " and a " + second_die.to_s + " for a total of " + sum.to_s + "."
@@ -25,8 +25,8 @@ get("/dice/2/6") do
 end
 
 get("/dice/2/10") do
-  first_die = rand(11)
-  second_die = rand(11)
+  first_die = rand(1..10)
+  second_die = rand(1..10)
   sum = first_die + second_die
 
   outcome = "You rolled a " + first_die.to_s + " and a " + second_die.to_s + " for a total of " + sum.to_s + "."
@@ -36,7 +36,7 @@ get("/dice/2/10") do
 end
 
 get("/dice/1/20") do
-  only_roll = rand(21)
+  only_roll = rand(1..20)
 
   outcome = "You rolled a " + only_roll.to_s + " for a total of " + only_roll.to_s + "."
 
@@ -45,11 +45,11 @@ get("/dice/1/20") do
 end
 
 get("/dice/5/4") do
-  roll_one = rand(5)
-  roll_two = rand(5)
-  roll_three = rand(5)
-  roll_four = rand(5)
-  roll_five = rand(5)
+  roll_one = rand(1..4)
+  roll_two = rand(1..4)
+  roll_three = rand(1..4)
+  roll_four = rand(1..4)
+  roll_five = rand(1..4)
   sum = roll_one + roll_two + roll_three + roll_four + roll_five
 
   outcome = "You rolled a " + roll_one.to_s + ", " + roll_two.to_s + ", " + roll_three.to_s + ", " + roll_four.to_s + ", and " + roll_five.to_s + " for a total of " + outcome.to_s + "."
